e8833e7e8a86b4eabdd4c8cc3d4500bd2adb5a23
Bewerber <bewerber@business-keeper.local>
2018-04-20 10:29:53 +0200

    added navigation

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default function() {
+return (
+     <header className="header">
+        <nav className="header__navigation">
+            <ul className="header__list">
+              <li className="header__list-item"><a className="header__anchor-tag" href="#" title="Start">Start</a></li>
+              <li className="header__list-item"><a className="header__anchor-tag" href="#" title="About">About</a></li>
+            </ul>
+        </nav>
+     </header>
+)
+}
